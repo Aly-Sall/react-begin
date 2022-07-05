400a1f40c7bfff756c1e1593fc811dbcaed45ba2
Aly-Sall <96480785+Aly-Sall@users.noreply.github.com>
2022-07-05 08:21:59 +0000

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "bootstrap/dist/css/bootstrap.css";
+
+const rootElement = document.querySelector("#root");
+//const myh4 = React.createElement("h1", null, "hello React");
+const Mydiv = () => (
+  <div>
+    <h4 className="alert alert-success display-4">Hello React</h4>
+    <ul type="square">
+      <li className="alert alert-warning">1 + 1 </li>
+      <li className="alert alert-warning">{1 + "1"}</li>
+      <li className="alert alert-warning">{1 + 1}</li>
+    </ul>
+  </div>
+);
+
+ReactDOM.render(<Mydiv />, rootElement);
